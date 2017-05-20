808e79152996f672b301e86e5f09e1707ead5f54
José soares <jsoares.z@outlook.com>
2017-05-20 11:36:06 +0100

    add travel done! #api #newtravel

--- routes/api.js
+++ routes/api.js
@@ -26,7 +26,43 @@ router.post('/v1/login', function(req, res, next){
     res.send(JSON.stringify({userid: docs._id, name: docs.name, age: docs.age}));
     return;
   });
-})
+});
+
+
+//add new travel to database
+router.post('/v1/travel', function(req, res, next){
+
+  //enable later! disabled while development
+  // if(!req.session.userid){
+  //   res.status('403').send(JSON.stringify({error: { code: "0x0000"}}));
+  //   return;
+  // }
+
+  var local = req.body.local;
+  var description = req.body.description;
+  var date = req.body.date;
+
+  if(!local || ((!local.city && !local.country) && !local.gps )|| !description || !date){
+    res.status('400').send(JSON.stringify({ error: { code: "0x0001"}}));
+    return;
+  }
+
+  var db = req.db;
+  var collection = db.get("travelcollection");
+
+  var inserted = true;
+
+  try {
+    collection.insert({userid: req.session.userid ? req.session.userid  : '1', description: description, date: date, local: local });
+  } catch (err) {
+    inserted = false;
+  }
+
+  if(inserted) res.status('201').send();
+  else res.status('409').send();
+
+  return;
+});
 
 
 module.exports = router;
