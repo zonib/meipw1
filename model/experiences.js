96e8080ed17a3cb84764b16c8f75485e414bf6fe
José soares <jsoares.z@outlook.com>
2017-06-03 10:46:30 +0100

    #p*tadavirgula

--- model/experiences.js
+++ model/experiences.js
@@ -7,7 +7,7 @@ var experienceSchema = new mongoose.Schema({
   gps: { lat: Number, lng: Number},
   medias: [mediaSchema],
   deleted: Boolean,
-  weather: { type: String, enum : ['Rain','Sunny'] }
+  weather: { type: String, enum : ['Rain','Sunny'] },
   classifications: {type: Array, default:[]}
 }, { _id: true } );
 
